4d8567f7846cff482327699a52cf7858aa039f7f
gbask <gbaskett@gmail.com>
2013-09-02 21:42:03 -0700

    trying out new connection attempt

--- app/server/modules/account-manager.js
+++ app/server/modules/account-manager.js
@@ -7,17 +7,27 @@ var moment 		= require('moment');
 var dbPort 		= 10033;
 var dbHost 		= 'paulo.mongohq.com';
 var dbName 		= 'local_ex_sample_1';
+var dbUserName	= 'gbask';
+var dbPassword	= 'tonys1';
 
 /* establish the database connection */
 
 var db = new MongoDB(dbName, new Server(dbHost, dbPort, {auto_reconnect: true}), {w: 1});
-	db.open(function(e, d){
-	if (e) {
-		console.log(e);
-	}	else{
-		console.log('connected to database :: ' + dbName);
-	}
-});
+	db.open( function(err) {
+		db.authenticate(
+			dbUserName,
+			dbPassword,
+			function(e, d){
+			if (e) {
+			console.log(e);
+			}	else{
+				console.log('connected to database :: ' + dbName);
+			}
+		}
+	);
+	
+	
+	});
 var accounts = db.collection('accounts');
 
 /* login validation methods */
